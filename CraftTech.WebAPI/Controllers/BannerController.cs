7700ea47598d1987e53d013fe24f90ac5ec981c3
n.fehmi26 <n.fehmi26@gmail.com>
2025-01-15 22:48:18 +0300

    Added: Banner Controller

---
+++ CraftTech.WebAPI/Controllers/BannerController.cs
@@ -0,0 +1,31 @@
+﻿using CraftTech.BussinessLayer.Abstract;
+using CraftTech.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CraftTech.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BannerController : ControllerBase
+    {
+        private readonly IBannerService _bannerService;
+
+        public BannerController(IBannerService bannerService)
+        {
+            _bannerService = bannerService;
+        }
+        [HttpGet]
+        public IActionResult BannerList()
+        {
+            var values = _bannerService.TGetList();
+            return Ok(values);
+        }
+        [HttpPost]
+        public IActionResult AddBanner(Banner banner)
+        {
+            _bannerService.TInsert(banner);
+            return Ok();
+        }
+    }
+}
